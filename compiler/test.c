2139440d4c459178c6d997950111cc6e66f30ef7
LeisurelyLucio <37182072+LeisurelyLucio@users.noreply.github.com>
2019-12-21 22:55:07 +0800

    结构体测试

--- compiler/test.c
+++ compiler/test.c
@@ -1,96 +1,18 @@
-int main(){
-    char expr[1000];
-    int st_num[1000];
-    char st_op[1000];
+#include  <string.h>
+#include  <stdlib.h>
 
-    int st_num_pt = -1;
-    int st_op_pt = -1;
+struct AVLNode{
+	int elem[100];
+	int height;
+};
 
-    gets(expr);
-    int len = strlen(expr);
-    for(int i = len-1; i >= 0; i = i - 1) {
-        expr[i + 1] = expr[i];
-    }
-    expr[0] = '(';
-    expr[len+1] = ')';
-    len = len + 2;
+struct AVLNode nodes[10000];
+struct AVLNode node;
 
-    i = len - 1;
-    int num = 0;
-    int k = 1;
-    while(i >= 0){
-        if(expr[i] == '+'){
-            while(st_op_pt >= 0 && ((st_op[st_op_pt] == '*') || (st_op[st_op_pt] == '/'))){
-                if(st_op[st_op_pt] == '*') {
-                    st_num[st_num_pt - 1] = st_num[st_num_pt] * st_num[st_num_pt - 1];
-                }
-                else {
-                    st_num[st_num_pt - 1] = st_num[st_num_pt] / st_num[st_num_pt - 1];
-                }
-                st_num_pt = st_num_pt - 1;
-                st_op_pt = st_op_pt - 1;
-            }
-            st_op_pt = st_op_pt + 1;
-            st_op[st_op_pt] = '+';
-            i = i - 1;
-        }else if(expr[i] == '-'){
-            while(st_op_pt >= 0 && ((st_op[st_op_pt] == '*') || (st_op[st_op_pt] == '/'))){
-                if(st_op[st_op_pt] == '*'){
-                    st_num[st_num_pt - 1] = st_num[st_num_pt] * st_num[st_num_pt - 1];
-                }
-                else{
-                    st_num[st_num_pt - 1] = st_num[st_num_pt] / st_num[st_num_pt - 1];
-                }
-                st_num_pt = st_num_pt - 1;
-                st_op_pt = st_op_pt - 1;
-            }
-            st_op_pt = st_op_pt + 1;
-            st_op[st_op_pt] = '-';
-            i = i - 1;
-        }else if(expr[i] == '*'){
-            st_op_pt = st_op_pt + 1;
-            st_op[st_op_pt] = '*';
-            i = i - 1;
-        }else if(expr[i] == '/'){
-            st_op_pt = st_op_pt + 1;
-            st_op[st_op_pt] = '/';
-            i = i - 1;
-        }else if(expr[i] == ')'){
-            st_op_pt = st_op_pt + 1;
-            st_op[st_op_pt] = ')';
-            i = i - 1;
-        }else if(expr[i] == '('){
-            while(st_op[st_op_pt] != ')'){
-                char ch = st_op[st_op_pt];
-                st_op_pt = st_op_pt - 1;
-                if(ch == '+'){
-                    st_num[st_num_pt - 1] = st_num[st_num_pt] + st_num[st_num_pt - 1];
-                }
-                else if(ch == '-'){
-                    st_num[st_num_pt - 1] = st_num[st_num_pt] - st_num[st_num_pt - 1];
-                }
-                else if(ch == '*'){
-                    st_num[st_num_pt - 1] = st_num[st_num_pt] * st_num[st_num_pt - 1];
-                }
-                else if(ch == '/'){
-                    st_num[st_num_pt - 1] = st_num[st_num_pt] / st_num[st_num_pt - 1];
-                }
-                st_num_pt = st_num_pt - 1;
-            }
-            st_op_pt = st_op_pt - 1;
-            i = i - 1;
-        }else{
-            num = 0;
-            k = 1;
-            while(i >= 0 && expr[i] >= '0' && expr[i] <= '9'){
-                num = num + (expr[i] - '0') * k;
-                k = k * 10;
-                i = i - 1;
-            }
-            st_num_pt = st_num_pt + 1;
-            st_num[st_num_pt] = num;
-        }
-    }
-    printf("%d\n", st_num[0]);
-    return 0;
+int main() {
+	node.height = 0;
+	nodes[1].elem[1] = 1;
+	nodes[1].height = 0;
+
+	return 0;
 }
